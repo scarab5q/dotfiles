e0501dc4c6a986e9987309f55df5b22ca9fb52a8
scarab5q <jackmdenny@gmail.com>
2023-07-16 14:27:30 +0100

    add new kickstart nvim config

---
+++ .config/nvim/lua/custom/plugins/neotree.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require('neo-tree').setup {}
+  end,
+  keys = {
+    "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree"
+  },
+}
